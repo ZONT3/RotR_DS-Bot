235bae437bb4743070559518b33a8502e29c6749
ZONT_ <danya10001@gmail.com>
2021-01-15 19:49:44 +0300

    Улучшение мониторинга

--- src/main/java/ru/zont/uinondsb/listeners/StatusMain.java
+++ src/main/java/ru/zont/uinondsb/listeners/StatusMain.java
@@ -5,7 +5,6 @@ import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 import ru.zont.dsbot.core.tools.Configs;
 import ru.zont.dsbot.core.tools.Strings;
-import ru.zont.uinondsb.command.Config;
 import ru.zont.uinondsb.tools.Commons;
 import ru.zont.uinondsb.tools.TStatus;
 
@@ -19,8 +18,12 @@ public class StatusMain extends ServerStatusEntry {
     @Override
     void update(Message entryMessage) {
         final TStatus.ServerInfoStruct inf = TStatus.retrieveInfo();
+        String d;
+        if (inf.time > 45000) d = ".d";
+        else d = "";
+
         entryMessage.getJDA().getPresence().setActivity(Activity.watching(String.format(
-                Strings.STR.getString("status.main.status"), inf.count, Configs.getPrefix())));
+                Strings.STR.getString("status.main.status" + d), inf.count, Configs.getPrefix())));
         entryMessage.editMessage(TStatus.Msg.status(inf, Commons.getRoleGmID())).complete();
     }
 
